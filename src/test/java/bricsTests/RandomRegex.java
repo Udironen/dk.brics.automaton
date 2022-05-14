b3e342ed48c5e52e41fd94c9dfaa67ae88936952
Udi Ronen <udir@ctera.com>
2022-05-15 01:32:28 +0300

    fixed bugs in union and RandomRegex and Validator

--- src/test/java/bricsTests/RandomRegex.java
+++ src/test/java/bricsTests/RandomRegex.java
@@ -1,8 +1,17 @@
 package test.java.bricsTests;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class RandomRegex {
+    private final String regex;
+    private final List<String> strings;
+
+    private RandomRegex(String regex, List<String> strings){
+        this.regex = regex;
+        this.strings = strings;
+    }
 
     public static String getRandString(){
         // create a string of all characters
@@ -36,8 +45,8 @@ public class RandomRegex {
         return sb.toString();
     }
 
-    public static char getRandRegexSign(){
-        String alphabet = "|?*+q";
+    public static char getRandBinarSign(){
+        String alphabet = "|q";
         Random random = new Random();
         return alphabet.charAt(random.nextInt(alphabet.length()));
     }
@@ -47,20 +56,94 @@ public class RandomRegex {
         return alphabet.charAt(random.nextInt(alphabet.length()));
     }
 
-    public static String getRandRegEx(){
+    public static RandomRegex getRandRegEx(){
+        List<String> genStrings = new ArrayList<>();
+        ArrayList<String> tempStrings = new ArrayList<>();
+        boolean toConcat = false;
         Random random = new Random();
         int numOfSingles = random.nextInt(3);
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < numOfSingles; ++i){
-            sb.append("((").append(getRandString()).append(")");
-            char sign = getRandUnarSign();
-            if (sign != 'q') sb.append(sign);
-            sb.append(")|");
+            String randString = getRandString();
+            if (!toConcat){
+                genStrings.addAll(tempStrings);
+                tempStrings = new ArrayList<>();
+                tempStrings.add(randString);
+            }
+            else {
+                for (int j = 0; j < tempStrings.size(); ++j) {
+                    tempStrings.set(j, tempStrings.get(j).concat(randString));
+                }
+            }
+            sb.append("((").append(randString).append(")");
+            char unarSign = getRandUnarSign();
+            if (unarSign != 'q') {
+                sb.append(unarSign);
+                for (int j = 1; j < 5; ++j) {
+                    if (!toConcat){
+                        tempStrings.add(randString.repeat(j+1));
+                    }
+                    else {
+                        if (tempStrings.size() <= j) {
+                            continue;
+                        }
+                        tempStrings.set(j, tempStrings.get(j).concat(randString.repeat(j + 1)));
+                    }
+                }
+            }
+            sb.append(")");
+            char binarSign = getRandBinarSign();
+            if (binarSign != 'q') {
+                toConcat = false;
+                sb.append(binarSign);
+            }
+            else toConcat = true;
         }
-        sb.append("(").append(getRandString()).append(")");
-        char sign = getRandUnarSign();
-        if (sign != 'q') sb.append(sign);
+        String randString = getRandString();
+        if (!toConcat){
+            genStrings.addAll(tempStrings);
+            tempStrings = new ArrayList<>();
+            tempStrings.add(randString);
+        }
+        else {
+            for (int j = 0; j < tempStrings.size(); ++j) {
+                tempStrings.set(j, tempStrings.get(j).concat(randString));
+            }
+        }
+        sb.append("(").append(randString).append(")");
+        char unarSign = getRandUnarSign();
+        if (unarSign != 'q') {
+            sb.append(unarSign);
+            for (int j = 1; j < 5; ++j) {
+                if (!toConcat){
+                    tempStrings.add(randString.repeat(j+1));
+                }
+                else {
+                    if (tempStrings.size() <= j) {
+                       continue;
+                    }
+                    tempStrings.set(j, tempStrings.get(j).concat(randString.repeat(j + 1)));
+                }
+            }
+        }
+        genStrings.addAll(tempStrings);
 
-        return sb.toString();
+        return new RandomRegex(sb.toString(), genStrings);
+    }
+
+    private static ArrayList<String> getEmptyStrings(){
+        ArrayList<String> emptyStrings = new ArrayList<>();
+        for (int i = 0; i < 5; ++i) {
+            emptyStrings.add("");
+        }
+        return emptyStrings;
+    }
+
+    public String getRegex() {
+        return regex;
+    }
+
+    public List<String> getStrings() {
+        return strings;
     }
 }
