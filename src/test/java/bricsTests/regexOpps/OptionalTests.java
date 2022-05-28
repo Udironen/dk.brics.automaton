4ade7f57b5c94a9185762b5476f72de389bb85c7
talyasapir2 <talyasapir2@gmail.com>
2022-05-28 13:35:17 +0300

    kinda final

--- src/test/java/bricsTests/regexOpps/OptionalTests.java
+++ src/test/java/bricsTests/regexOpps/OptionalTests.java
@@ -3,6 +3,8 @@ package test.java.bricsTests.regexOpps;
 import dk.brics.automaton.Automaton;
 import dk.brics.automaton.BasicOperations;
 import dk.brics.automaton.RegExp;
+import dk.brics.automaton.State;
+import dk.brics.automaton.Transition;
 import test.java.bricsTests.RandomRegex;
 import test.java.bricsTests.Validator;
 
@@ -37,7 +39,26 @@ public class OptionalTests {
         	Automaton opt = BasicOperations.optional(a);
         	Boolean bool = (!(a.getShortestExample(true).equals("")))&&((opt.getShortestExample(true).equals("")));
             validator.addCheck(bool , "failed on regex: " + reg1.getRegex());
+            bool = (opt.minus(Automaton.makeEmptyString()).equals(a));
+            validator.addCheck(bool , "a.opt()-'' is not equal to a: " + reg1.getRegex());
         }
         Assert.assertTrue(validator.getMessage(), validator.isValid());
+	}
+	
+	//WHITE BOX TESTS
+	@Test
+	public void initialShouldRejectTest()
+	{
+		State init = new State();
+    	State second = new State();
+    	init.setAccept(false);
+    	second.setAccept(true);
+    	init.addTransition(new Transition('0', second));
+    	second.addTransition(new Transition('1', init));
+    	Automaton automatonA = new Automaton();
+    	automatonA.setInitialState(init);
+		automatonA =  BasicOperations.optional(automatonA);
+		Assert.assertFalse("it accepts a string i used to not accept", BasicOperations.run(automatonA, "01"));
+		
 	}
 }
